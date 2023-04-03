d0369650ccd69b586495339b16e51cb688c4ff9d
Deivid <90482796+Deivimm@users.noreply.github.com>
2023-04-03 18:56:05 -0300

    feat(navbar): adiciona componente navbar e configura suas rotas
    - Cria o componente navbar com seu HTML e CSS
    - Configura as rotas para acessar o componente home
    
    Refs #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './navBar/navBar.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HomeComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
